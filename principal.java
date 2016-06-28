ea1b234cb2afe12b2448749907c03dfff423b8cb
lorensov <lucianlorens@gmail.com>
2016-06-28 11:49:25 -0200

    Update principal.java
    Removing unnecessary comments

--- principal.java
+++ principal.java
@@ -4,36 +4,23 @@ import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.Arrays;
-
-
 /**
  * @Job for Lacuna Software... DevTest contained in: http://devtest.lacunasoftware.com/#/home
  * @author lorensov
  */
 public class principal {
-
     /**
      * @param args lol
-     * 1[OK] Estabilish a TCP connection with the server
-     * 2[OK] Receive a message encoded with "T-Scheme"
-     * 3[] Validade the received message according to "T-Scheme"
-     * 4[] Break the cipher contained in the message
-     * 5[] Encode the revealed with "T-Scheme"
-     * 6[] Send response to server
-     * 7[] Receive, validade and decrypt the confirmation message from server according to "T-Scheme" and the key used previously to encrypt the first message.
+     * 1o Estabilish a TCP connection with the server
+     * 2o Receive a message encoded with "T-Scheme"
+     * 3o Validade the received message according to "T-Scheme"
+     * 4o Break the cipher contained in the message
+     * 5o Encode the revealed with "T-Scheme"
+     * 6o Send response to server
+     * 7o Receive, validade and decrypt the confirmation message from server according to "T-Scheme" and the key used previously to encrypt the first message.
      */
-    
-    public static String digest(String lol){
-        //receive md5 in lol
-        
-        
-        return lol;
-    }
-            
-    
     public static void main(String[] args) throws UnsupportedEncodingException {
         //1
-        
           System.out.println("Starting Communication...");
         Socket MyClient = null;
         DataInputStream input = null;
@@ -53,7 +40,7 @@ public class principal {
    if (MyClient != null && output != null && input != null){
     try{
         System.out.println("The message from the host is:");
-        while ((responseLine = input.readLine()) != null) {System.out.print("Server: " + responseLine);// if (responseLine.indexOf("OK") != -1) {break;} }
+        while ((responseLine = input.readLine()) != null) {System.out.print("Server: " + responseLine);
         System.out.println();
         
         d = new byte [responseLine.length()];
@@ -101,14 +88,9 @@ public class principal {
      System.out.print("The parity Value is: "+parity);
      System.out.println(" ");
     
-    //fazer md5 checking?
-    
-    //if array md5 == md5generated_from_data[] print("ok");
-    
     //4 Break the cipher contained in the message
     
     //Análise de Frequência
-   // int[][] most; most = new int[bg][2];  
     byte[] moist = new byte[bg];
     int [] moistCounter = new int [bg];
             
@@ -118,12 +100,6 @@ public class principal {
         for (j=0; j < bg; j++){
         if (j<i && data[i] == data[j]){break;}
         if (data[j] == data[i]){counter++;}      
-        //if (j == dg-1){System.out.println("the byte: "+ data[i]+" is present "+counter+" times");}
-        //declara no vetor que está do lado de fora...
-        
-       // most [i][0] = data[i];
-        //most [i][1] = counter;
-        
         moist [i] = data[i];
         moistCounter [i] = counter;
 
@@ -133,24 +109,6 @@ public class principal {
         //most counters
         int x,y,aux=0;
         byte baux=0;
-        /**
-            for (x=0;x<most.length;x++)
-            {
-                for (y=0;y<most.length;y++)
-                {
-                    if(most[x][1] > most[y][1])
-                    {
-                        //declaração de contador
-                        aux = most[x][1];
-                        most[x][1]=most[y][1];
-                        most[y][1]=aux;
-                        //declaração de byte
-                        aux = most[x][0];
-                        most[x][0]=most[y][0];
-                        most[y][0]=aux;    
-                    }
-                } 
-            } */
             
             //bubble sort de moist, diferenciando byte de int
                         for (x=0;x<bg;x++)
@@ -174,15 +132,12 @@ public class principal {
              //**********printagem de most**********
             System.out.println("Segue abaixo a ordem de bytes de acordo com sua frequência");
         System.out.println("======");
-     /** for (int p=0; p<most.length;p++){ if (most[p][1] != 0) System.out.println("O byte "+most[p][0]+" ocorre: "+most[p][1]); }     
-     System.out.println("O caralho do Byte mais comum é o: "+most[0][0]+" que ocorre: "+most[0][1]+" vezes CARALHO!");*/
      
       //**********printagem de moist**********
       for (int p=0; p<moist.length;p++){ if (moistCounter[p] != 0) System.out.println("O byte "+moist[p]+" ocorre: "+moistCounter[p]+"x"); }    
-     System.out.println("O caralho do Byte mais comum é o: "+moist[0]+" que ocorre: "+moistCounter[0]+" vezes CARALHO!");
+     System.out.println("O caralho do Byte mais comum é o: "+moist[0]+" que ocorre: "+moistCounter[0]+" vezes!");
       
-
-//**********printagem da mensagem**********
+    //**********printagem da mensagem**********
      String CodeMessage = new String(data, "US-ASCII");
      System.out.println("Este é a String da Array data[]:");
      System.out.println(CodeMessage);
@@ -201,33 +156,26 @@ public class principal {
         //subtrair da string em todos of valores de byte[]d
        for (i=0;i<data.length;i++){ data[i] = (byte) (data[i] ^ key);}
         //printar em string nova mensagem alterada
-    // for (i=0;i<data.length;i++){System.out.print(data[i]);}
+    
      System.out.println();
      System.out.println("Este é o NOVO valor da String da data[]");
      System.out.println(Arrays.toString(data)); 
      String deCodeMessage = new String(data, "US-ASCII");
      System.out.println("Este é a NOVA String da Array data[]:");
      System.out.println(deCodeMessage);
-     
-     
-     
-     
         }
      }catch (IOException f){System.err.println("No Message Received");}
-   } 
-    
-   
-   
-   
-     //5 Encode the revealed with "T-Scheme"
-        //message length
-        //do the md5 digest of the message
-        //data encoded in plain text
-        //parity changed to 01
-     
-     
-     //6 Send response to server
-     //7 Receive, validade and decrypt the confirmation message from server according to "T-Scheme" and the key used previously to encrypt the first message.
+   }
+ /**yet to implement::
+     5 Encode the revealed with "T-Scheme"
+     problems...
+        message length
+        do the md5 digest of the message
+        data encoded in plain text
+        parity changed to 01
+     6 Send response to server
+     7 Receive, validade and decrypt the confirmation message from server according to "T-Scheme" and 
+     the key used previously to encrypt the first message. */
  
         System.out.println("lorensov qualified code!");
 }
